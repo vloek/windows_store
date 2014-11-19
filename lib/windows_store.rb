6f7cfc1d0ec517f0edfb7a0ab558e3f95da5afc2
Lex <lex.antonov@gmail.com>
2014-11-19 15:37:08 +0300

    Update windows_store.rb

--- lib/windows_store.rb
+++ lib/windows_store.rb
@@ -6,9 +6,6 @@ require 'json'
 
 module WindowsStore
 
-  # SECRET = 'KABs25LN%2FbQ6ZOPKAHTAmACnNnhxEso2'
-  # APP_ID = 'ms-app://s-1-15-2-2687536063-3587835031-342050253-1892685130-1179825253-3067520980-1590187497'
-
   def self.verify!(reciept, secret, app_id)
     reciept     = reciept.gsub(/^\s+/, '').gsub(/\s+$/, '').gsub(/\n/, '')
     signed_doc  = Xmldsig::SignedDocument.new(reciept)
